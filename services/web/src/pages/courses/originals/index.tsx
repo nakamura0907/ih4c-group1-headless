7a46ba18a87518929c805c73283941fac360460a
nakamura0907 <nakamura990907@gmail.com>
2023-07-01 20:23:24 +0900

    オリジナルコース一覧画面エラーハンドリング

--- services/web/src/pages/courses/originals/index.tsx
+++ services/web/src/pages/courses/originals/index.tsx
@@ -1,30 +1,53 @@
-import React from "react";
+import { Course } from "@/features/course";
+import { errorHandler, fetch } from "@/utils/fetcher/strapi";
 import { NextPage } from "next";
+import { Pagination, message } from "antd";
+import { SpotImage, SpotList } from "@/features/spot";
+import { translateStrapiSpotToSpot } from "@/features/spot/api/strapi";
+import { useRouter } from "next/router";
 import Layout from "@/components/template/layout";
 import Link from "next/link";
-import { fetch } from "@/utils/fetcher/strapi";
-import { translateStrapiSpotToSpot } from "@/features/spot/api/strapi";
-import { SpotImage, SpotList } from "@/features/spot";
-import { Course } from "@/features/course";
+import React from "react";
 
 type State = {
   courses: Course[];
+  pagination: {
+    current: number;
+    pageSize: number;
+    total: number;
+  };
 };
 
 const initialState: State = {
   courses: [],
+  pagination: {
+    current: 1,
+    pageSize: 25,
+    total: 0,
+  },
 };
 
 const OriginalCourseList: NextPage = () => {
+  const router = useRouter();
+
   const [courses, setCourses] = React.useState(initialState.courses);
+  const [pagination, setPagination] = React.useState(initialState.pagination);
 
   /**
    * オリジナルコース一覧を取得する
    */
   React.useEffect(() => {
     (async () => {
+      if (!router.isReady) return;
+
+      const { page } = router.query;
       const response = await fetch.get(
-        "/original-courses?populate[spots][populate][0]=photo,category"
+        "/original-courses?populate[spots][populate][0]=photo,category",
+        {
+          params: {
+            "pagination[page]": isNaN(Number(page)) ? 1 : Number(page),
+          },
+        }
       );
       const courses = response.data.data.map((course: any) => {
         const route = course.attributes.spots.data.map(
@@ -36,9 +59,23 @@ const OriginalCourseList: NextPage = () => {
           route,
         };
       });
+
       setCourses(courses);
-    })();
-  }, []);
+      setPagination({
+        current: response.data.meta.pagination.page,
+        pageSize: response.data.meta.pagination.pageSize,
+        total: response.data.meta.pagination.total,
+      });
+    })().catch((error) => {
+      const msg = "オリジナルコース一覧の取得に失敗しました";
+      if (errorHandler(error)) {
+        message.error(msg);
+        return;
+      }
+      if (error.response.status === 404) return;
+      message.error(error.response.data.message || msg);
+    });
+  }, [router]);
 
   return (
     <Layout>
@@ -61,6 +98,21 @@ const OriginalCourseList: NextPage = () => {
           );
         })}
       </SpotList>
+      <Pagination
+        current={pagination.current}
+        pageSize={pagination.pageSize}
+        total={pagination.total}
+        onChange={(page) => {
+          router.push(
+            {
+              pathname: router.pathname,
+              query: { ...router.query, page },
+            },
+            undefined,
+            { shallow: true }
+          );
+        }}
+      />
     </Layout>
   );
 };
