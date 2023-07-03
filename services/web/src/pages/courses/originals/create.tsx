3d00f9a48429b09088029215a9bebfac7b2c8aa0
nakamura0907 <nakamura990907@gmail.com>
2023-07-03 16:28:32 +0900

    オリジナルコース作成画面

--- services/web/src/pages/courses/originals/create.tsx
+++ services/web/src/pages/courses/originals/create.tsx
@@ -1,36 +1,109 @@
 import { NextPage } from "next";
 import { PrimaryButton } from "@/components/ui/button";
-import { Spot, SpotImage, SpotList, services } from "@/features/spot";
-import { strapiBaseUrl, strapiToken } from "@/utils/fetcher/strapi";
+import {
+  CategorySelect,
+  Spot,
+  SpotImage,
+  SpotList,
+  defaultCategory,
+} from "@/features/spot";
+import {
+  StrapiGetEntriesResponse,
+  StrapiSpot,
+  errorHandler,
+  strapiBaseUrl,
+  strapiToken,
+} from "@/utils/fetcher/strapi";
 import axios from "axios";
 import Layout from "@/components/template/layout";
 import React from "react";
+import { useRouter } from "next/router";
+import { Badge, Form, Input, Pagination, message } from "antd";
+import { fetch } from "@/utils/fetcher/strapi";
+import { translateStrapiSpotToSpot } from "@/features/spot/api/strapi";
+
+type RefactorResponse = StrapiGetEntriesResponse<StrapiSpot>;
 
 type State = {
   spots: Spot[];
   selectedSpots: string[];
+  category: string;
+  pagination: {
+    current: number;
+    pageSize: number;
+    total: number;
+  };
 };
 
 const initialState: State = {
   spots: [],
   selectedSpots: [],
+  category: defaultCategory.value,
+  pagination: {
+    current: 1,
+    pageSize: 25,
+    total: 0,
+  },
+};
+
+type FormValues = {
+  name: string;
 };
 
 const OriginalCourseCreate: NextPage = () => {
+  const router = useRouter();
+  const [form] = Form.useForm<FormValues>();
+
   const [spots, setSpots] = React.useState(initialState.spots);
   const [selectedSpots, setSelectedSpots] = React.useState(
     initialState.selectedSpots
   );
+  const [category, setCategory] = React.useState(initialState.category);
+  const [pagination, setPagination] = React.useState(initialState.pagination);
 
   /**
    * 観光スポット一覧を取得する
    */
   React.useEffect(() => {
     (async () => {
-      const spots = await services.fetchSpotList();
+      if (!router.isReady) return;
+
+      const { page, category } = router.query;
+      const response = await fetch.get<RefactorResponse>("/spots", {
+        params: {
+          populate: "photo,category",
+          "pagination[page]": isNaN(Number(page)) ? 1 : Number(page),
+          filters: {
+            category:
+              category === "*"
+                ? undefined
+                : {
+                    id: {
+                      $eq: category,
+                    },
+                  },
+          },
+        },
+      });
+      const spots = response.data.data.map(translateStrapiSpotToSpot);
+
       setSpots(spots);
-    })();
-  }, []);
+      if (category) setCategory(category.toString());
+      setPagination({
+        current: response.data.meta.pagination.page,
+        pageSize: response.data.meta.pagination.pageSize,
+        total: response.data.meta.pagination.total,
+      });
+    })().catch((error) => {
+      const msg = "観光スポット一覧の取得に失敗しました。";
+      if (errorHandler(error)) {
+        message.error(msg);
+        return;
+      }
+      if (error.response.status === 404) return;
+      message.error(error.response.data.message || msg);
+    });
+  }, [router]);
 
   /**
    * オリジナルコースに追加するスポットを選択する
@@ -68,29 +141,101 @@ const OriginalCourseCreate: NextPage = () => {
     );
 
     console.log(response);
+    setSelectedSpots(initialState.selectedSpots);
+  };
+
+  const handleFinish = async (values: FormValues) => {
+    const url = `${strapiBaseUrl}/original-courses`;
+    const response = await axios.post(
+      url,
+      {
+        data: {
+          name: values.name,
+          spots: {
+            connect: selectedSpots.map((spotId) => spotId),
+          },
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${strapiToken}`,
+        },
+      }
+    );
+
+    setSelectedSpots(initialState.selectedSpots);
+    form.resetFields();
+
+    router.push(`/courses/originals/${response.data.data.id}`);
   };
 
   return (
     <Layout>
-      <p>デバッグ用: 選択したスポット {selectedSpots.join(",")}</p>
       <h2>オリジナルコース</h2>
-      <select>
-        <option>カテゴリ選択</option>
-      </select>
+      <CategorySelect
+        value={category}
+        onChange={(value) => {
+          router.push(
+            {
+              pathname: router.pathname,
+              query: { ...router.query, category: value, page: 1 },
+            },
+            undefined,
+            { shallow: true }
+          );
+        }}
+      />
       <SpotList>
-        {spots.map((spot) => (
-          <li key={spot.id} onClick={() => toggleSpotSelection(spot.id)}>
-            <SpotImage src="/no-image.png" alt={spot.name} />
-            <span>{spot.name}</span>
-          </li>
-        ))}
+        {spots.map((spot) => {
+          const index = selectedSpots.indexOf(spot.id);
+          return (
+            <li key={spot.id} onClick={() => toggleSpotSelection(spot.id)}>
+              <SelectBadge index={index + 1}>
+                <SpotImage src="/no-image.png" alt={spot.name} />
+              </SelectBadge>
+              <span>{spot.name}</span>
+            </li>
+          );
+        })}
       </SpotList>
-      <div>ページネーション</div>
-      <PrimaryButton onClick={handleCreateOriginalCourse}>
-        オリジナルコースの作成
-      </PrimaryButton>
+      <Pagination
+        current={pagination.current}
+        pageSize={pagination.pageSize}
+        total={pagination.total}
+        onChange={(page) => {
+          router.push(
+            {
+              pathname: router.pathname,
+              query: { ...router.query, page },
+            },
+            undefined,
+            { shallow: true }
+          );
+        }}
+      />
+      <Form form={form} onFinish={handleFinish}>
+        <Form.Item label="オリジナルコース名" name="name" required>
+          <Input placeholder="オリジナルコース名" />
+        </Form.Item>
+        <Form.Item>
+          <PrimaryButton disabled={selectedSpots.length == 0} htmlType="submit">
+            オリジナルコースの作成
+          </PrimaryButton>
+        </Form.Item>
+      </Form>
     </Layout>
   );
+};
+
+type SelectBadgeProps = {
+  index: number;
+  children: React.ReactNode;
+};
+const SelectBadge: React.FC<SelectBadgeProps> = (props) => {
+  const { index, children } = props;
+
+  if (index <= 0) return <>{children}</>;
+  return <Badge count={index}>{children}</Badge>;
 };
 
 export default OriginalCourseCreate;
