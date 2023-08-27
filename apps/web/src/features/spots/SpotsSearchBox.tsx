e5d8088bbe58f2aa111dc796c51db7cc16182fbc
nakamura0907 <nakamura990907@gmail.com>
2023-08-27 23:20:53 +0900

    Web: 観光スポット検索フォームコンポーネント作成

--- apps/web/src/features/spots/SpotsSearchBox.tsx
+++ apps/web/src/features/spots/SpotsSearchBox.tsx
@@ -1,18 +1,47 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useCategorySelect } from "../categories/useCategorySelect"
+import { Button, Container, Flex, TextInput } from "@mantine/core";
+import { useForm } from "@/hooks/useMantine";
 
+type FormValues = {
+    q?: string;
+}
+
+/**
+ * 観光スポット検索フォーム
+ */
 export const SpotsSearchBox = () => {
-    const searchParams = useSearchParams();
+    const router = useRouter();
+    const pathname = usePathname() ?? "/"
 
+    const searchParams = useSearchParams();
+    const qSearchParam = searchParams?.get("q") ?? undefined;
     const categorySearchParam = searchParams?.get("category") ?? undefined;
+
     const { current, CategorySelect } = useCategorySelect(categorySearchParam);
 
+    const form = useForm<FormValues>({
+        initialValues: {
+            q: qSearchParam
+        }
+    })
+
+    const handleSubmit = (values: FormValues) => {
+        const query = `q=${values.q}&category=${current}&page=1`;
+        router.push(`${pathname}?${query}`)
+    }
+
     return(
-        <div>
-            検索エリア
-            <CategorySelect />
-        </div>
+        <Container size="xs">
+            <form onSubmit={form.onSubmit(handleSubmit)}>
+                <Flex>
+                    <TextInput placeholder="XX博物館" style={{ width: "100%"}} {...form.getInputProps("q")} />
+                    <Button type="submit">検索</Button>
+                </Flex>
+                <CategorySelect />
+            </form>
+        </Container>
     )
 }
