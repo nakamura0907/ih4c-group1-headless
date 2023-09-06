b1891a58c816c43001a8f861824132def2b31c3a
nakamura0907 <nakamura990907@gmail.com>
2023-09-06 21:34:00 +0900

    Web: 観光スポット検索フォームコンポーネントのテスト作成

---
+++ apps/web/src/features/spots/SpotsSearchForm/SpotsSearchForm.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { SpotsSearchForm } from "./index";
+import { usePathname, useRouter } from "next/navigation";
+import { useCategorySelector } from "@/features/categories";
+
+// next/navigationのモック
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+  usePathname: jest.fn(),
+  useSearchParams: jest.fn(),
+}));
+const pushMock = jest.fn();
+(useRouter as jest.Mock).mockReturnValue({
+  push: pushMock,
+});
+(usePathname as jest.Mock).mockReturnValue("/test-pathname");
+
+// @/features/categoriesのモック
+jest.mock("@/features/categories", () => ({
+  useCategorySelector: jest.fn(),
+}));
+(useCategorySelector as jest.Mock).mockReturnValue({
+  current: "testCategory",
+  CategorySelector: () => <div>CategorySelector</div>,
+});
+
+describe("検索ボタンが押下される", () => {
+  it("正しいURLにページ遷移する", () => {
+    // イベント準備
+    render(<SpotsSearchForm />);
+
+    const input = screen.getByPlaceholderText("XX博物館");
+    fireEvent.change(input, { target: { value: "testQuery" } });
+
+    // Clickイベント発火
+    const searchButton = screen.getByText("検索");
+    fireEvent.click(searchButton);
+
+    // ページ遷移の引数が正しいこと
+    expect(pushMock).toHaveBeenCalledWith(
+      "/test-pathname?q=testQuery&category=testCategory&page=1",
+    );
+  });
+});
